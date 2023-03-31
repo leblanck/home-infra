d53102099b0e237a2d6e1e7b789f782715fbeb0b
Kyle LeBlanc <kyle@leblanc.sh>
2023-03-31 09:44:48 -0400

    Update dnsCheck.sh

--- dnsCheck.sh
+++ dnsCheck.sh
@@ -3,8 +3,11 @@
 #######
 # Checking Home Public IP Address and comparing to DNS.
 # If public DNS has changed then we need to update DNS via Netlify to point to new public IP. 
-# This shuold be run internally. It will fail if run externaly
-#######
+# This shuold be run internally. It will fail if run externaly.
+###################
+# Running via cronjob as follows:
+# 0 8 * * * /usr/bin/dnsCheck (Runs everyday at 8 AM)
+###################
 
 # Colors
 BGreen='\033[1;32m'
@@ -12,10 +15,11 @@ BRed='\033[1;31m'
 NC='\033[0m'
 
 SLACK_WEBHOOK_URL=''
-SLACK_CHANNEL=''
+SLACK_CHANNEL='#alerts'
 
+#Change to Known Good Public IP
 setIP='127.0.0.1'
-currentIP='dig +short myip.opendns.com @resolver1.opendns.com'
+currentIP=$(curl -s https://ipecho.net/plain ; echo)
 
 send_notification() {
   local color='good'
@@ -30,9 +34,9 @@ send_notification() {
 }
 
 if [[ $setIP == "$currentIP" ]]; then
-    echo -e "${BGreen}SUCCESS!${NC} - Current Public IP is set correctly. No Change Needed."
+    echo -e "${BGreen}SUCCESS!${NC} - Current Public IP is set correctly. No Change Needed. Public IP: $currentIP"
     send_notification 'INFO' "SUCCESS!" "Current Public IP is set correctly. No Change Needed."
 else
-    echo -e "${BRed}FAILURE${NC} - Current Public IP is NOT correct, adjust in Netlify ASAP"
+    echo -e "${BRed}FAILURE${NC} - Current Public IP is NOT correct, adjust in Netlify ASAP. Public IP: $currentIP"
     send_notification 'ERROR' "ERROR - DNS MISMATCH" "Current Public IP is NOT correct, adjust in Netlify ASAP"
 fi
